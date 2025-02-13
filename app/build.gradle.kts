f07ea0c544f9bd8f9d4b3fb7afef5fdc6d64c704
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-13 16:02:03 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,52 +1,32 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
-    id("maven-publish")
+    id("com.android.application")
+    id("kotlin-android")
 }
 
 android {
-    compileSdk = libs.versions.compile_sdk_version.get().toInt()
-
+    compileSdkVersion(34)
     defaultConfig {
-        minSdk = libs.versions.min_sdk_version.get().toInt()
-        namespace = "com.ncorti.kotlin.template.library.android"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+        applicationId = "com.example.app"
+        minSdkVersion(23)
+        targetSdkVersion(34)
+        versionCode = 1
+        versionName = "1.0"
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
-    }
-    lint {
-        warningsAsErrors = true
-        abortOnError = true
-        disable.add("GradleDependency")
-    }
-}
-
-publishing {
-    singleVariant("release") {
-        withSourcesJar()
-        withJavadocJar()
     }
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.core.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.runner)
-    androidTestImplementation(libs.androidx.test.ext.junit)
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.9.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
