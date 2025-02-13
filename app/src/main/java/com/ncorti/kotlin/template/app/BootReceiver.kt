71358cdef3a5e04134efab9b11ef81fb0c8e5fe8
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-13 16:56:19 +0530

    Update BootReceiver.kt

--- app/src/main/java/com/ncorti/kotlin/template/app/BootReceiver.kt
+++ app/src/main/java/com/ncorti/kotlin/template/app/BootReceiver.kt
@@ -3,12 +3,17 @@ package com.ncorti.kotlin.template.app
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action.equals(Intent.ACTION_BOOT_COMPLETED)) {
             val serviceIntent = Intent(context, BackgroundService::class.java)
-            context.startForegroundService(serviceIntent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
         }
     }
 }
