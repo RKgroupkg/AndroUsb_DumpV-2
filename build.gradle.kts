6f4fe3e9a4daf9bc681fb6ba7b2ad78d9a4e306f
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-13 16:08:27 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion by extra("1.9.0")
-    val agpVersion by extra("8.0.2")
-
     repositories {
         google()
         mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:$agpVersion")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("com.android.application") version "8.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
