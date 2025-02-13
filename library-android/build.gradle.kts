91c959097ffa863d2d7183b8ca9eaf9abbe9d728
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-13 10:23:35 +0530

    Update build.gradle.kts

--- library-android/build.gradle.kts
+++ library-android/build.gradle.kts
@@ -1,32 +1,25 @@
-version = LibraryAndroidCoordinates.LIBRARY_VERSION
-
 plugins {
     id("com.android.library")
     kotlin("android")
     id("maven-publish")
-    publish
 }
 
 android {
-    compileSdk = libs.versions.compile.sdk.version.get().toInt()
+    compileSdk = libs.versions.compile_sdk_version.get().toInt()
 
     defaultConfig {
-        minSdk = libs.versions.min.sdk.version.get().toInt()
+        minSdk = libs.versions.min_sdk_version.get().toInt()
         namespace = "com.ncorti.kotlin.template.library.android"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
-
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
@@ -36,14 +29,6 @@ android {
             )
         }
     }
-
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-            withJavadocJar()
-        }
-    }
-
     lint {
         warningsAsErrors = true
         abortOnError = true
@@ -51,12 +36,17 @@ android {
     }
 }
 
+publishing {
+    singleVariant("release") {
+        withSourcesJar()
+        withJavadocJar()
+    }
+}
+
 dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
-
     testImplementation(libs.junit)
-
     androidTestImplementation(libs.androidx.test.runner)
     androidTestImplementation(libs.androidx.test.ext.junit)
 }
