41d4933c75323106cad759e1811ad9a4e1a38950
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-13 16:28:36 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ buildscript {
     val kotlinVersion = "1.9.0"
     val agpVersion = "8.0.2"
     
+    repositories {  // Add this repositories block back in buildscript
+        google()
+        mavenCentral()
+    }
+    
     dependencies {
         classpath("com.android.tools.build:gradle:$agpVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
