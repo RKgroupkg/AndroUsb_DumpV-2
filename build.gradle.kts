09e6e22a51c04f3c5eb76c0ddba81f2d5822ea3e
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-13 16:24:19 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,22 +2,9 @@ buildscript {
     val kotlinVersion = "1.9.0"
     val agpVersion = "8.0.2"
     
-    repositories {
-        google()
-        mavenCentral()
-    }
     dependencies {
         classpath("com.android.tools.build:gradle:$agpVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    }
-}
-
-// Remove the plugins block as we're using buildscript block
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
 
