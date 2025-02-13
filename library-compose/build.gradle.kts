5b30eac55c9740ae0ecb873bc1705d4b3cadb44d
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-13 10:24:01 +0530

    Update build.gradle.kts

--- library-compose/build.gradle.kts
+++ library-compose/build.gradle.kts
@@ -1,39 +1,31 @@
 plugins {
-    id("com.android.library")
     kotlin("android")
     alias(libs.plugins.compose.compiler)
-
 }
 
 android {
-    compileSdk = libs.versions.compile.sdk.version.get().toInt()
+    compileSdk = libs.versions.compile_sdk_version.get().toInt()
 
     defaultConfig {
-        minSdk = libs.versions.min.sdk.version.get().toInt()
+        minSdk = libs.versions.min_sdk_version.get().toInt()
         namespace = "com.ncorti.kotlin.template.library.compose"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
         vectorDrawables {
             useSupportLibrary = true
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
-
     buildFeatures {
         compose = true
         viewBinding = true
         buildConfig = false
     }
-
     lint {
         warningsAsErrors = true
         abortOnError = true
@@ -45,16 +37,13 @@ dependencies {
     implementation(projects.libraryKotlin)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
-
     implementation(platform(libs.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling)
     implementation(libs.compose.foundation)
     implementation(libs.compose.material)
-
     testImplementation(libs.junit)
-
     debugImplementation(libs.compose.ui.test.manifest)
     androidTestImplementation(libs.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.test.runner)
