9c35849105137ff3686e3617d90418bb74b42781
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-14 22:11:18 +0530

    Update MainActivity.kt

--- app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
+++ app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import android.os.Environment
 import android.provider.Settings
 import android.widget.Button
